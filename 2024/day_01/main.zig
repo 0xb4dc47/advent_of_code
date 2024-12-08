963d321ce94c73dc1aca16f26f6e8845f24d7067
0xbadbad <lpenapenn@gmail.com>
2024-12-08 01:27:33 -0500

    day 1 part 1 complete

--- 2024/day_01/main.zig
+++ 2024/day_01/main.zig
@@ -6,11 +6,15 @@ const allocator = std.heap.page_allocator;
 
 const line_ending = if (builtin.os.tag == .windows) "\r\n" else "\n";
 
+const ListsDifferentSizeError = error{
+    UnevenLists,
+};
+
 pub fn main() !void {
     var lines = std.mem.tokenizeScalar(u8, input_file, '\n');
 
-    var column1 = std.ArrayList(i64).init(allocator);
-    var column2 = std.ArrayList(i64).init(allocator);
+    var column1 = std.ArrayList(i32).init(allocator);
+    var column2 = std.ArrayList(i32).init(allocator);
     defer column1.deinit();
     defer column2.deinit();
 
@@ -18,18 +22,26 @@ pub fn main() !void {
         var split_line = std.mem.tokenizeScalar(u8, line, ' ');
         if (split_line.next()) |data1| {
             if (split_line.next()) |data2| {
-                const parsed_data1 = try std.fmt.parseInt(i32, data1, 10) catch continue;
-                const parsed_data2 = try std.fmt.parseInt(i32, data2, 10) catch continue;
+                const parsed_data1 = try std.fmt.parseInt(i32, data1, 10);
+                const parsed_data2 = try std.fmt.parseInt(i32, data2, 10);
                 try column1.append(parsed_data1);
                 try column2.append(parsed_data2);
             }
         }
-        // print("1: {i}, 2: {}\n", .{ column1.getLast(), column2.getLast() });
-        // const data1 = split_line.next().?;
-        // const data2 = split_line.next().?;
-        // std.mem.sort( type, items: []T, context: anytype, comptime lessThanFn: fn(@TypeOf(context), lhs:T, rhs:T)bool)
     }
 
-    // print("column1: {s}\n", .{olumn1.items});
-    // print("column2: {s}\n", .{column2.items});
+    std.mem.sort(i32, column1.items, {}, comptime std.sort.asc(i32));
+    std.mem.sort(i32, column2.items, {}, comptime std.sort.asc(i32));
+
+    if (column1.items.len != column2.items.len) return ListsDifferentSizeError.UnevenLists;
+    var sum: i64 = 0;
+    for (column1.items, 0..) |item1, index| {
+        const item2 = column2.items[index];
+
+        const difference = @abs(item1 - item2);
+
+        sum += difference;
+    }
+
+    print("The sum is: {d}", .{sum});
 }
