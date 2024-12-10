6989ae324013b0d0e234f1bcb9e08341e05a3443
0xbadbad <lpenapenn@gmail.com>
2024-12-10 17:34:29 -0500

    new approach

--- 2024/day_02/part2.zig
+++ 2024/day_02/part2.zig
@@ -15,51 +15,73 @@ pub fn main() !void {
     var totalCount: i32 = 0;
 
     while (lines.next()) |line| {
-        // split to get an iterator for the line
-        var splitLine = std.mem.tokenizeScalar(u8, line, ' ');
+        // var splitLine = std.mem.tokenizeScalar(u8, line, ' ');
+        // for (splitLine)
 
-        // set the initial direction
-        var direction: directions = directions.undetermined;
+        const isSafe = try isLineSafe(line);
 
-        // all lines are safe by default
-        var isSafe: bool = true;
-
-        var prev: i32 = try std.fmt.parseInt(i32, splitLine.next().?, 10);
-
-        while (splitLine.next()) |num| {
-            const curr = try std.fmt.parseInt(i32, num, 10);
-
-            // first pass of a line the direction is undetermined
-            if (direction == directions.undetermined) {
-                if (curr > prev) {
-                    direction = directions.increasing;
-                } else if (curr < prev) {
-                    direction = directions.decreasing;
-                } else {
-                    isSafe = false;
-                    break;
-                }
-            }
-
-            // check if the direction is maintained for this window
-            const isIncreasing: bool = curr > prev;
-            const isDecreasing: bool = curr < prev;
-
-            const isWrongDirection: bool = (direction == directions.increasing and isDecreasing) or (direction == directions.decreasing and isIncreasing);
-
-            if (!isDifferInRange(prev, curr) or isWrongDirection) {
-                isSafe = false;
-                break;
-            }
-            prev = curr;
+        if (!isSafe) {
+            unsafeCount += 1;
         }
+
         totalCount += 1;
-        if (!isSafe) unsafeCount += 1;
     }
 
     print("Total Lines: {d}\n", .{totalCount});
     print("Safe Lines: {d}\n", .{totalCount - unsafeCount});
     print("Unsafe Lines: {d}\n", .{unsafeCount});
+}
+
+pub fn isLineSafe(line: []const u8) !bool {
+    // pub fn isLineSafe(splitLine: std.mem.TokenIterator(u8, std.mem.DelimiterType.scalar)) !bool {
+    // split to get an iterator for the line
+    var splitLine = std.mem.tokenize(u8, line, ' ');
+
+    // set the initial direction
+    var direction: directions = directions.undetermined;
+
+    // all lines are safe and dampener is available by default
+    var isDampenerAvailable: bool = true;
+
+    // var prev: i32 = try std.fmt.parseInt(i32, splitLine.next().?, 10);
+    var prev: i32 = splitLine.next().?;
+
+    while (splitLine.next()) |num| {
+        const curr = try std.fmt.parseInt(i32, num, 10);
+
+        const isIncreasing: bool = curr > prev;
+        const isDecreasing: bool = curr < prev;
+
+        // first pass of a line the direction is undetermined
+        if (direction == directions.undetermined) {
+            if (isIncreasing) {
+                direction = directions.increasing;
+            } else if (isDecreasing) {
+                direction = directions.decreasing;
+            } else {
+                if (isDampenerAvailable) {
+                    isDampenerAvailable = false;
+                    continue;
+                }
+                return false;
+            }
+        }
+
+        // check if the direction is maintained for this window
+        const isWrongDirection: bool = (direction == directions.increasing and isDecreasing) or (direction == directions.decreasing and isIncreasing);
+
+        if (!isDifferInRange(prev, curr) or isWrongDirection) {
+            if (isDampenerAvailable) {
+                isDampenerAvailable = false;
+                continue;
+            }
+            return false;
+        }
+
+        prev = curr;
+    }
+
+    return true;
 }
 
 pub fn isDifferInRange(a: i32, b: i32) bool {
